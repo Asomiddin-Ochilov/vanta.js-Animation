2f26bbd4d24e359dbb9fb9dd7f91790a3ad0944c
Asomiddin <asomhon12345@gmail.com>
2022-06-11 09:18:21 +0500

    porgress vanta.js

---
+++ app.js
@@ -0,0 +1,17 @@
+VANTA.BIRDS({
+     el: "#s-page-1",
+     mouseControls: true,
+     touchControls: true,
+     gyroControls: false,
+     minHeight: 625.00,
+     minWidth: 200.00,
+     scale: 1.00,
+     scaleMobile: 1.00,
+     color1: 0x4a2222,
+     birdSize: 2.10,
+     wingSpan: 37.00,
+     speedLimit: 8.00,
+     separation: 57.00,
+     alignment: 45.00,
+     cohesion: 55.00
+   })
